f4a7b9a1d0c198a3b216057e32e6c42d5056dc50
Marco <marco.carlomagno@correo.ucu.edu.uy>
2025-08-18 10:58:46 -0300

    STATIC y mayusculas

--- src/Program/Multiplication.cs
+++ src/Program/Multiplication.cs
@@ -3,7 +3,7 @@ namespace Ucu.Poo.GitChallenge;
 public class Multiplication
 
 {
-public int Multiply(int a, int b)
+public static int Multiply(int a, int b)
 {
     return a * b;
 }
--- src/Program/subtraction.cs
+++
@@ -1,9 +0,0 @@
-namespace Ucu.Poo.GitChallenge;
-
-public class Subtraction
-{
-    public static int Subtract(int a, int b)
-    {
-        return a - b
-    }
-}
