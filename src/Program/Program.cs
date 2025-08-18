7c1d65f3ee9ff3d45844e92c21380b2a2ba2c4eb
Fedebap <tigrus245@gmail.com>
2025-08-18 14:21:31 -0300

    nueva operacion

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -4,3 +4,11 @@ Console.WriteLine(Addition.Add(7, 2));
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
+
+public class Power
+{
+    public static int Squared(int a)
+    {
+        return a * a;
+    }
+}
